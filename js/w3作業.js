90b7fad59290197f7baaa033b9424b23cc12e35b
joyce001121 <39973753+joyce001121@users.noreply.github.com>
2020-06-17 15:37:03 +0800

    a
    a

--- js/w3作業.js
+++ js/w3作業.js
@@ -3,4 +3,15 @@ $('.ham-icon').click(function(e) {
     e.preventDefault();
     $('.dropdown').toggleClass('active')
   })
+
+// QA: arrow轉180度/collapse顯示
+$(".qa-item-detail").click(function() {
+   $(this).parent().toggleClass("show");
+});
+
+
+
+
+  
+
   
